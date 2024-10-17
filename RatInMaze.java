6af18fbec23ae34c4c12dc3829a87516c01328d0
ShrijanSingh <workshrijan8756@gmail.com>
2024-10-17 11:56:20 +0530

    Added Printing of Path

--- RatInMaze.java
+++ RatInMaze.java
@@ -1,25 +1,26 @@
 import java.util.*;
+
 public class RatInMaze {
-    public static void main(String [] args)
-    {
-        int n=0;
-        int m=0;
+    public static void main(String[] args) {
+        int n = 0;
+        int m = 0;
         System.out.println("Enter row and coulumn of matrix");
-        Scanner sc=new Scanner(System.in);
-        n=sc.nextInt();
-        m=sc.nextInt();
-        int res=noOfWays(1,1,n,m);
-        System.out.println("No of ways="+res);
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        noOfWays(1, 1, n, m, "");
+        // System.out.println("No of ways="+res);
     }
-    public static int noOfWays(int sr,int sc,int er,int ec)
-    {
-        if (sr>er || sc>ec)
-        return 0;
-        if (sr==er && sc==ec)
-        return 1;
-        int downways=noOfWays(sr+1,sc,er,ec);
-        int rightways=noOfWays(sr,sc+1,er,ec);
-       int  totalways=downways+rightways;
-        return totalways;
-}
+
+    public static void noOfWays(int sr, int sc, int er, int ec, String s) {
+        if (sr > er || sc > ec)
+            return;
+        if (sr == er && sc == ec) {
+            System.out.println(s);
+            return;
+        }
+        noOfWays(sr + 1, sc, er, ec, s + "D");
+        noOfWays(sr, sc + 1, er, ec, s + "R");
+
+    }
 }
